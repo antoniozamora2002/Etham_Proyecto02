d78bf0a2e1fc5ec6a90af46a9ae4204088627c41
Marco <marco.rios@upeu.edu.pe>
2022-11-14 17:53:47 -0500

    Programas con angular

--- src/main/java/com/ayllusinchi/upeu/Controller/ProgramaController.java
+++ src/main/java/com/ayllusinchi/upeu/Controller/ProgramaController.java
@@ -4,9 +4,12 @@ import com.ayllusinchi.upeu.Services.ProgramaService;
 import com.ayllusinchi.upeu.entidades.Programa;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author etham
  */
-
+//Agregado en clase de angular
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/programas")
 @Api(value = "Microservicio de Gestion de los programas", description = "Microservicio de Gestion de los programas")
@@ -30,9 +34,13 @@ public class ProgramaController {
     ProgramaService programaService;
     
     @ApiOperation(value = "Lista de programas")
-    @GetMapping("/all")
-    public List<Programa> findAll(){
-        return programaService.findAll();
+    @GetMapping()
+    public ResponseEntity<?> findAll(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("message", "Lista de Personas");
+        result.put("data", programaService.findAll());
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
     
     @ApiOperation(value = "Obtiene datos de un programa")
