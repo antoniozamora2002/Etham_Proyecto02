66ee471a6a76f29dac4734fee6b9b9797981d5f4
antoniozamora2002 <antonio.zamora@upeu.edu.pe>
2022-11-29 17:09:50 -0500

    Facultad, Tipopersona, Carrera - sus controllers

--- src/main/java/com/ayllusinchi/upeu/Controller/CarreraController.java
+++ src/main/java/com/ayllusinchi/upeu/Controller/CarreraController.java
@@ -2,12 +2,14 @@ package com.ayllusinchi.upeu.Controller;
 
 import com.ayllusinchi.upeu.Services.CarreraService;
 import com.ayllusinchi.upeu.entidades.Carrera;
-import com.ayllusinchi.upeu.entidades.Facultad;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author etham
  */
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/carrera")
 @Api(value = "Microservicio de Gestion de las carreras", description = "Microservicio de Gestion de las carreras")
@@ -30,35 +33,72 @@ public class CarreraController {
     @Autowired
     CarreraService carreraService;
     
-    @ApiOperation(value = "Lista de carreras")
-    @GetMapping("/all")
-    public List<Carrera> findAll(){
-        return carreraService.findAll();
+    @ApiOperation(value = "Lista de facultades")
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("message", "Lista de facultades");
+        result.put("data", carreraService.findAll());
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    
-    @ApiOperation(value = "Obtiene datos de una carrera")
+
+    @ApiOperation(value = "Obtiene datos de facultades")
     @GetMapping("/{id}")
-    public ResponseEntity<Carrera> findById(@PathVariable Long id){
+    public ResponseEntity<Carrera> findById(@PathVariable Long id) {
         Carrera carrera = carreraService.findById(id);
         return ResponseEntity.ok(carrera);
     }
+
     
-    @ApiOperation(value = "Crea una carrera")
-    @PostMapping("/save")
-    public Carrera save(@RequestBody Carrera carrera){
-        return carreraService.save(carrera);
+    @ApiOperation(value = "Crea una facultad")
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Carrera carrera) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("message", "Facultad registrado correctamente");
+        result.put("data", carreraService.save(carrera));
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    
+    @ApiOperation(value = "Modifica una facultad")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody Carrera carrera) {
+        HashMap<String, Object> result = new HashMap<>();
+        Carrera data = carreraService.findById(id);
+        if (data == null) {
+            result.put("success", false);
+            result.put("message", "No existe registro con Id: " + id);
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+        try {
+            carrera.setCaId(id);
+            carreraService.save(carrera);
+            result.put("success", true);
+            result.put("message", "Datos actualizados correctamente.");
+            result.put("data", carrera);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(new Exception(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
-    @ApiOperation(value = "Modifica una carrera")
-    @PutMapping("/update")
-    public Carrera update(@RequestBody Carrera carrera){
-        return carreraService.save(carrera);
-    }
-    
-    @ApiOperation(value = "Elimina una carrera")
-    @DeleteMapping("/id")
-    public void deleteById(@PathVariable Long id){
-        carreraService.deleteById(id);
+    @ApiOperation(value = "Elimina una facultad")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        HashMap<String, Object> result = new HashMap<>();
+    Carrera data = carreraService.findById(id);
+    if(data == null){
+        result.put("success", false);
+        result.put("message", "No existe programa con id:" + id);
+  return new ResponseEntity <>(result, HttpStatus.NOT_FOUND);
+    } else{
+  carreraService.deleteById(id);
+            result.put("success", true);
+            result.put("message", "Registro Eliminado correctamente");
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
     }
     
 }
--- src/main/java/com/ayllusinchi/upeu/Controller/TipoPersonaController.java
+++ src/main/java/com/ayllusinchi/upeu/Controller/TipoPersonaController.java
@@ -4,9 +4,12 @@ import com.ayllusinchi.upeu.Services.TipoPersonaService;
 import com.ayllusinchi.upeu.entidades.TipoPersona;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,43 +24,81 @@ import org.springframework.web.bind.annotation.RestController;
  * @author etham
  */
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
-@RequestMapping("/tipopersona")
-@Api(value = "Microservicio de Gestion de los tipo de personas", description = "Microservicio de Gestion de los tipo de personas")
+@RequestMapping("/tipopersonas")
+@Api(value = "Microservicio de Gestion de tipo de personas", description = "Microservicio de Gestion de tipo de personas")
 public class TipoPersonaController {
     
     @Autowired
     TipoPersonaService tipoPersonaService;
     
-    @ApiOperation(value = "Lista de un tipo")
-    @GetMapping("/all")
-    public List<TipoPersona> findAll(){
-        return tipoPersonaService.findAll();
+    @ApiOperation(value = "Lista de tipo de personas")
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("message", "Lista de tipo de personas");
+        result.put("data", tipoPersonaService.findAll());
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    
-    @ApiOperation(value = "Obtiene datos de un tipo")
+
+    @ApiOperation(value = "Obtiene datos de tipo de personas")
     @GetMapping("/{id}")
-    public ResponseEntity<TipoPersona> findById(@PathVariable Long id){
+    public ResponseEntity<TipoPersona> findById(@PathVariable Long id) {
         TipoPersona tipoPersona = tipoPersonaService.findById(id);
         return ResponseEntity.ok(tipoPersona);
     }
+
     
-    @ApiOperation(value = "Crea un tipo")
-    @PostMapping("/save")
-    public TipoPersona save(@RequestBody TipoPersona tipoPersona){
-        return tipoPersonaService.save(tipoPersona);
+    @ApiOperation(value = "Crea un tipo de persona")
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody TipoPersona tipoPersona) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("message", "Programa registrado correctamente");
+        result.put("data", tipoPersonaService.save(tipoPersona));
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    
+    @ApiOperation(value = "Modifica un tipo de persona")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody TipoPersona tipoPersona) {
+        HashMap<String, Object> result = new HashMap<>();
+        TipoPersona data = tipoPersonaService.findById(id);
+        if (data == null) {
+            result.put("success", false);
+            result.put("message", "No existe registro con Id: " + id);
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+        try {
+            tipoPersona.setTpId(id);
+            tipoPersonaService.save(tipoPersona);
+            result.put("success", true);
+            result.put("message", "Datos actualizados correctamente.");
+            result.put("data", tipoPersona);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(new Exception(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
-    @ApiOperation(value = "Modifica un tipo")
-    @PutMapping("/update")
-    public TipoPersona update(@RequestBody TipoPersona tipoPersona){
-        return tipoPersonaService.save(tipoPersona);
-    }
-    
-    @ApiOperation(value = "Elimina un tipo")
-    @DeleteMapping("/id")
-    public void deleteById(@PathVariable Long id){
-        tipoPersonaService.deleteById(id);
+    @ApiOperation(value = "Elimina un tipo de persona")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        HashMap<String, Object> result = new HashMap<>();
+    TipoPersona data = tipoPersonaService.findById(id);
+    if(data == null){
+        result.put("success", false);
+        result.put("message", "No existe programa con id:" + id);
+  return new ResponseEntity <>(result, HttpStatus.NOT_FOUND);
+    } else{
+  tipoPersonaService.deleteById(id);
+            result.put("success", true);
+            result.put("message", "Registro Eliminado correctamente");
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
     }
     
 }
